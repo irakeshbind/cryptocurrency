855107adb5443e4d1b6a53dff8534d9b177f5400
irakeshbind <rakeshbind351998@gmail.com>
2025-11-05 10:10:36 +0530

    create server

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
+
