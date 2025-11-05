d905ec863a8daa96623e32809b91857203d73b3b
irakeshbind <rakeshbind351998@gmail.com>
2025-11-05 10:23:04 +0530

    routes create

---
+++ backend/routes/coins.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const axios = require("axios");
+const CurrentData = require("../models/CurrentData");
+const HistoryData = require("../models/HistoryData");
+const router = express.Router();
+
+
+module.exports = router;
+
